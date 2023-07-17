8364134cc3f00c78087def15fc8f9d439d9841ee
davhyandrade <david.souza95@etec.sp.gov.br>
2023-07-17 20:35:52 -0300

    fix: ix favicon bug caused by outdated Next.js version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,6 @@ import Head from 'next/head'
 function MyApp({ Component, pageProps }: AppProps) {
   return(
     <>
-      <Head>
-        <link rel="shortcut icon" href="https://i.postimg.cc/1Rgvfws7/hostmain.png" type="image/x-icon" /> { /* Tentei fazer do modo convencional, mas nao funcionou, nao estava reconhecendo o arquivo favicon.ico, portanto eu acabei colocando o link da imagem */ }
-      </Head>
       <Context>
         <Component {...pageProps} />
       </Context>
