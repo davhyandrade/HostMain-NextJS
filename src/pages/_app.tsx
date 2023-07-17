9a26f26b5cdb434ce6c4a542a4c71b76b02b94d7
davhyandrade <david.souza95@etec.sp.gov.br>
2023-07-17 20:36:57 -0300

    fix: fix favicon bug caused by outdated Next.js version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import Context from '../context/layout'
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
-import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
